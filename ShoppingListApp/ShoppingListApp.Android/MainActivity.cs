54c49592be0e8b3c0cde10ef56eeb6ad5ef70b05
tuxmainy <magic-shoppinglist@damage.devloop.de>
2021-02-13 22:29:30 +0100

    xamarin forms 5

--- ShoppingListApp/ShoppingListApp.Android/MainActivity.cs
+++ ShoppingListApp/ShoppingListApp.Android/MainActivity.cs
@@ -19,8 +19,6 @@ namespace ShoppingListApp.Droid
 
             base.OnCreate(savedInstanceState);
 
-            global::Xamarin.Forms.Forms.SetFlags("SwipeView_Experimental");
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
--- ShoppingListApp/ShoppingListApp/Services/IShoppingListDataStore.cs
+++ ShoppingListApp/ShoppingListApp/Services/IShoppingListDataStore.cs
@@ -6,18 +6,22 @@ namespace ShoppingListApp.Services
 {
     public interface IShoppingListDataStore
     {
+        /* GET */
         Task<IEnumerable<ShoppingList>> GetShoppingListsAsync();
         Task<ShoppingList> GetShoppingListAsync(string shoppingListId);
         Task<IEnumerable<ShoppingItem>> GetShoppingItemsAsync(string shoppingListId);
         Task<StoreItem> GetStoreItemAsync(string itemId);
         Task<IEnumerable<StoreItem>> GetStoreItemsAsync();
 
+        /* SEARCH */
         Task<IEnumerable<StoreItem>> SearchStoreItemsAsync(string text);
 
+        /* ADD */
         Task<string> AddShoppingListAsync(ShoppingList shoppingList);
         Task<string> AddShoppingItemAsync(string shoppingListId, ShoppingItem shoppingItem);
         Task<string> AddStoreItemAsync(StoreItem storeItem);
 
+        /* REMOVE */
         Task<ShoppingList> RemoveShoppingListAsync(string shoppingListId);
         Task<ShoppingItem> RemoveShoppingListItemAsync(string shoppingListId, string shoppingItemId);
     }
